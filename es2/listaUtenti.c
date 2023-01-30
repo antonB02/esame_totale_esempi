443a0da1d149a3c08072b7399369f13ddf0460fc
antonB02 <anton.buchkov@edu.unife.it>
2023-01-30 15:02:51 +0100

    slight corrections

--- es2/listaUtenti.c
+++ es2/listaUtenti.c
@@ -35,71 +35,64 @@ void insTesta(Lista *pl, Utente utente)
 
 void aggiorna(Lista *pl, char cf[], int attivita)
 {
-    while (*pl)
+    while (*pl && strcmp((*pl)->dati.cf, cf) != 0)
     {
-        if (strcmp((*pl)->dati.cf, cf) != 0)
+        pl = &(*pl)->next;
+    }
+
+    if ((*pl)->dati.disabled)
+    {
+        printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
+    }
+
+    if (attivita == 1)
+    {
+        if ((*pl)->dati.cont1 != 0)
         {
-            pl = &(*pl)->next;
+            (*pl)->dati.cont1--;
         }
         else
         {
-            if ((*pl)->dati.disabled)
+            printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
+            (*pl)->dati.accessi_negati++;
+            if ((*pl)->dati.accessi_negati == 3)
             {
-                printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
+                (*pl)->dati.disabled = 1;
             }
+        }
+    }
 
-            if (attivita == 1)
+    else if (attivita == 2)
+    {
+        if ((*pl)->dati.cont2 != 0)
+        {
+            (*pl)->dati.cont2--;
+        }
+        else
+        {
+            printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
+            (*pl)->dati.accessi_negati++;
+            if ((*pl)->dati.accessi_negati == 3)
             {
-                if ((*pl)->dati.cont1 != 0)
-                {
-                    (*pl)->dati.cont1--;
-                }
-                else
-                {
-                    printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
-                    (*pl)->dati.accessi_negati++;
-                    if ((*pl)->dati.accessi_negati == 3)
-                    {
-                        (*pl)->dati.disabled = 1;
-                    }
-                }
+                (*pl)->dati.disabled = 1;
             }
+        }
+    }
 
-            else if (attivita == 2)
+    else if (attivita == 3)
+    {
+        if ((*pl)->dati.cont3 != 0)
+        {
+            (*pl)->dati.cont3--;
+        }
+        else
+        {
+            printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
+            (*pl)->dati.accessi_negati++;
+            if ((*pl)->dati.accessi_negati == 3)
             {
-                if ((*pl)->dati.cont2 != 0)
-                {
-                    (*pl)->dati.cont2--;
-                }
-                else
-                {
-                    printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
-                    (*pl)->dati.accessi_negati++;
-                    if ((*pl)->dati.accessi_negati == 3)
-                    {
-                        (*pl)->dati.disabled = 1;
-                    }
-                }
+                (*pl)->dati.disabled = 1;
             }
-
-            else if (attivita == 3)
-            {
-                if ((*pl)->dati.cont3 != 0)
-                {
-                    (*pl)->dati.cont3--;
-                }
-                else
-                {
-                    printf("Codice Fiscale %s, attivita `%d: Accesso negato\n", (*pl)->dati.cf, attivita);
-                    (*pl)->dati.accessi_negati++;
-                    if ((*pl)->dati.accessi_negati == 3)
-                    {
-                        (*pl)->dati.disabled = 1;
-                    }
-                }
-            }
-
-            pl = &(*pl)->next;
         }
     }
 }
